4417a7eafb467b9f82df08e7ef62616468c2c5c9
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2019-09-03 10:43:50 -0700

    Agregar archivos de proyecto.

---
+++ GraficadorSeñales/Sinc.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraficadorSeñales
+{
+    class Sinc
+    {
+        public List<Muestra> Muestras { get; set; }
+
+        public Sinc()
+        {
+            Muestras = new List<Muestra>();
+        }
+        public double evaluar(double tiempo)
+        {
+            double resultado;
+            resultado = (Math.Sin(Math.PI * tiempo)) / (Math.PI * tiempo);
+
+            return resultado;
+        }
+    }
+}
